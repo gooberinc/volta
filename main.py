81413a1696d2e9cc85e9a5b17cf7e7c7082a3444
WhatDidYouExpect <89535984+WhatDidYouExpect@users.noreply.github.com>
2025-07-07 00:33:28 +0200

    no one hears the words, they say.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # If you're seeing this after cloning the Goober repo, note that this is a standalone module for translations.
 # While it's used by Goober Core, it lives in its own repository and should not be modified here.
 # For updates or contributions, visit: https://github.com/gooberinc/volta
-
+# Also, Note to self: Add more comments it needs more love
 import os
 import json
 import pathlib
